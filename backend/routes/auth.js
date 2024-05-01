76408aa06c06d51a1bb554b7732c896ebe41f49e
shraddesh123 <shraddheshdeshmukh23@gmail.com>
2024-05-01 13:46:15 +0530

    s

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,9 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-
+JWT_SECRETE = 'THUNDER'
 // Create a User using: POST "/api/auth/createuser". Doesn't require Auth
 router.post('/createuser', [
   body('name', 'Enter a valid name').isLength({ min: 3 }),
@@ -23,18 +24,27 @@ router.post('/createuser', [
 
   //it is known as hashing for the security of passwords 
   //we have used bcryptjs to gena\erate the hash password
-  const salt= await bcrypt.genSalt(10);
-  secPass= await bcrypt.hash(req.body.password,salt);
+  const salt = await bcrypt.genSalt(10);
+  secPass = await bcrypt.hash(req.body.password, salt);
   user = await User.create({
     name: req.body.name,
     password: secPass,
     email: req.body.email,
   })
-    .then(user => res.json(user))
-    .catch(err => {
-      console.log(err)
-      res.json({ error: 'Please enter a unique value for email', message: err.message })
-    })
+  const data = {
+    user:{
+      id:user.id
+    }
+  }
+  const authToken = jwt.sign(data, JWT_SECRETE);
+  console.log(authToken);
+  try {
+    res.json(authToken);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'An error occurred', message: err.message });
+  }
+  
 })
 
 module.exports = router
